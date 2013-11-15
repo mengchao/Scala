f9a6e531b294fa92fc2d30ac4939b9dffa564aec
Chao Meng <mengchao.agile@gmail.com>
2013-11-15 18:14:10 +0800

    Simulations Assignment
    Implement "Or Gates"

---
+++ simulations/src/test/scala/simulations/CircuitSuite.scala
@@ -0,0 +1,68 @@
+package simulations
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CircuitSuite extends CircuitSimulator with FunSuite {
+  val InverterDelay = 1
+  val AndGateDelay = 3
+  val OrGateDelay = 5
+  
+  test("andGate example") {
+    val in1, in2, out = new Wire
+    andGate(in1, in2, out)
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+    
+    assert(out.getSignal === false, "and 1")
+
+    in1.setSignal(true)
+    run
+    
+    assert(out.getSignal === false, "and 2")
+
+    in2.setSignal(true)
+    run
+    
+    assert(out.getSignal === true, "and 3")
+  }
+  
+  test("orGate example") {
+    val in1, in2, out = new Wire
+    orGate(in1, in2, out)
+    testOrLogic(in1, in2, out)
+  }
+
+  test("orGate2 example") {
+    val in1, in2, out = new Wire
+    orGate2(in1, in2, out)
+    testOrLogic(in1, in2, out)
+  } 
+  
+  def testOrLogic(in1: Wire, in2: Wire, out: Wire): Unit = {
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+    
+    assert(out.getSignal === false, "or 1")
+
+    in1.setSignal(true)
+    run
+    
+    assert(out.getSignal === true, "or 2")
+
+    in2.setSignal(true)
+    run
+    
+    assert(out.getSignal === true, "or 3")
+  }
+    
+  //
+  // to complete with tests for orGate, demux, ...
+  //
+
+}
